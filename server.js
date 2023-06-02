83cc29d94104290af3adf16956f420618d4618c7
Ankush kumar <ankushkumar11903@gmail.com>
2023-06-02 12:41:13 +0530

    new.js

--- server.js
+++ server.js
@@ -1,106 +1,20 @@
-const express = require('express');
-const cors = require('cors');
 const fetch = require('cross-fetch');
+const cors=require('cors');
+const express=require('express');
 
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
-});
+const app=express();
 app.use(cors());
-app.get('/', (req, res) => { 
-    res.json({"test":"hello Foodsale users !!! "});
-  });
+const port=process.env.PORT || 5000;
 
+app.get('/search',async(req,res)=>{
+    const {value}=req.query;
+    const response=await fetch(`https://api.edamam.com/search?app_id=900da95e&app_key=40698503668e0bb3897581f4766d77f9&q=${value}`);
+    const data=await response.json();
+    res.json(data);
+}
+);
 
-  app.get('/api/restaurants', (req, res) => {
-    const url = `https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6126255&lng=77.04108959999999&page_type=DESKTOP_WEB_LISTING`;
-  
-    fetch(url, {
-      headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
-      }
-    })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then(data => {
-        res.json(data);
-      })
-      .catch(error => {
-        console.error(error);
-        res.status(500).send('An error occurred');
-      });
-  });
-
-
-
-  app.get('/api/restaurants2', (req, res) => {
-    const { cart } = req.query;
-    // console.log(req.query);
-    const url = `https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6126255&lng=77.04108959999999&offset=${cart}&sortBy=RELEVANCE&pageType=SEE_ALL&page_type=DESKTOP_SEE_ALL_LISTING`;
-  
-    fetch(url, {
-      headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
-      }
-    })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then(data => {
-        res.json(data);
-      })
-      .catch(error => {
-        console.error(error);
-        res.status(500).send('An error occurred');
-      });
-  });
-
-
-  app.get('/api/menu', (req, res) => {
-    const { restaurantId } = req.query;
-    console.log(req.query);
-  
-  
-    // const url = `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.6126255&lng=77.04108959999999&restaurantId=${restaurantId}`;
-
-
-    const url=  `https://api.edamam.com/search?app_id=900da95e&app_key=40698503668e0bb3897581f4766d77f9&q=${restaurantId}`;
-  
-  
-    fetch(url, {
-      headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
-      
-      }
-    })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then(data => {
-        res.json(data);
-      })
-      .catch(error => {
-        console.error(error);
-        res.status(500).send('An error occurred');
-      });
-  });
-  
-  
+app.listen(port,()=>{
+    console.log('Server is running on port 5000');
+}
+);
